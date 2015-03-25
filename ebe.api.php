c7262ce6606cda86b80ed84ad52f14030a9d9d1f
Erik Seifert <erik.seifert@b-connect.de>
2015-03-25 23:35:08 +0100

    Add api documentation

---
+++ ebe.api.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Alter function for graph informations
+ * 
+ * $context
+ *   - entity_type
+ *     The entity type 
+ *   - bundle
+ *     Current bundle
+ *   - entity_info 
+ *     Entity info for current bundle
+ * 
+ * @param unknown $graph_info
+ * @param unknown $context
+ */
+function my_module_graph_info_alter($graph_info,$context){
+	
+}
